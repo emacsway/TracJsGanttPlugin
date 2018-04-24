029829b95e78320e2d3633a2704404ba5bbcf603
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2018-04-24 18:25:26 +0000

    Trac-jsGantt 1.2.0.0dev: Branch for Trac 1.2 and conform to PEP8
    Refs #13175.

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2010-2014 Chris Nelson <Chris.Nelson@SIXNET.com>
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+
+from setuptools import setup
+
+setup(
+    name='Trac-jsGantt',
+    author='Chris Nelson',
+    author_email='Chris.Nelson@SIXNET.com',
+    description='Trac plugin displaying jsGantt charts in Trac',
+    version='1.2.0.0',
+    url='https://trac-hacks.org/wiki/TracJsGanttPlugin',
+    license='3-Clause BSD',
+    packages=['tracjsgantt'],
+    package_data={'tracjsgantt': ['htdocs/*.js', 'htdocs/*.css']},
+    entry_points={
+        'trac.plugins': [
+            'tracjsgantt = tracjsgantt'
+        ]
+    }
+)
